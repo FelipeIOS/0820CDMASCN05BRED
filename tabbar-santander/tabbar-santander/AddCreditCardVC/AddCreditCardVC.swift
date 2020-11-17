30147dc4e4ee5c7d09e4c8ce42cd120489b354ac
felipe silva <felipe.instrutor.ios@gmail.com>
2020-11-16 22:44:47 -0300

    implementei a camera

--- tabbar-santander/tabbar-santander/AddCreditCardVC/AddCreditCardVC.swift
+++ tabbar-santander/tabbar-santander/AddCreditCardVC/AddCreditCardVC.swift
@@ -27,21 +27,39 @@ class AddCreditCardVC: UIViewController {
     @IBOutlet weak var photoButton: UIButton!
     
     @IBOutlet weak var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.nameTextField.delegate = self
-        self.numberCardTextField.delegate = self
-        self.dateTextField.delegate = self
+        self.configDelegates()
+        self.configLayoutScreen()
         
-        self.configScreen()
+    
         // Do any additional setup after loading the view.
     }
     
-    private func configScreen() {
+    private func configDelegates() {
+        self.nameTextField.delegate = self
+        self.numberCardTextField.delegate = self
+        self.dateTextField.delegate = self
+    }
+    
+    private func configLayoutScreen() {
         
         self.saveButton.layer.cornerRadius = 4
         self.photoButton.layer.cornerRadius = 4
+        
+        self.nameTextField.layer.borderWidth = 0.5
+        self.numberCardTextField.layer.borderWidth = 0.5
+        self.dateTextField.layer.borderWidth = 0.5
+        
+        self.nameTextField.layer.borderColor = UIColor.red.cgColor
+        self.numberCardTextField.layer.borderColor = UIColor.red.cgColor
+        self.dateTextField.layer.borderColor = UIColor.red.cgColor
+       
+        self.nameTextField.layer.cornerRadius = 4
+        self.numberCardTextField.layer.cornerRadius = 4
+        self.dateTextField.layer.cornerRadius = 4
     }
     
     @IBAction func tappedSelectFlagSegmented(_ sender: UISegmentedControl) {
@@ -49,6 +67,7 @@ class AddCreditCardVC: UIViewController {
     
     @IBAction func tappedPhotoButton(_ sender: UIButton) {
      
+        self.getImage(fromSourceType: .photoLibrary)
     }
 
     @IBAction func tappedSaveButton(_ sender: UIButton) {
@@ -101,6 +120,37 @@ class AddCreditCardVC: UIViewController {
         }
         
         return true
+    }
+    
+
+    private func getImage(fromSourceType sourceType: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = sourceType
+            
+            self.present(imagePickerController, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    
+}
+
+extension AddCreditCardVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let urlImage:NSURL? = info[UIImagePickerController.InfoKey.imageURL] as? NSURL
+        
+        print("urlImage:\(urlImage)")
+        
+        print("=========\(info)")
+        picker.dismiss(animated: true, completion: nil)
+        
     }
 }
 
