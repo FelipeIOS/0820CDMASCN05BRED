c2cfd16659b921c4ccd3c440405d0eb279c2fd7b
Felipe Miranda <felipe.instrutor.ios@gmail.com>
2021-01-12 19:39:04 -0300

    update code

--- tabbar-santander/tabbar-santander/Locais/LocaisVC.swift
+++ tabbar-santander/tabbar-santander/Locais/LocaisVC.swift
@@ -16,58 +16,18 @@ class LocaisVC: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let initialLocation = CLLocation(latitude: -23.565163997932217, longitude: -46.652365089520536)
-        self.centerMapOnLocation(location: initialLocation)
-        
-        
-       
-        
-        
-        let agencia: Agencia = Agencia(title: "Trianon", subtitle: "lugar turistico", categoria: "teste", lat: "-23.565163997932217", lng: "-46.652365089520536")
-                                       
-        self.myMapView.addAnnotation(agencia)
-        
        
         
         // Do any additional setup after loading the view.
     }
-    
-    func centerMapOnLocation(location: CLLocation) {
-        
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: self.regionRadius, longitudinalMeters: self.regionRadius)
-        self.myMapView.setRegion(coordinateRegion, animated: true)
-        
-    }
-    
-    let locationManager = CLLocationManager()
-    
-    func checkLocationAuthorizationStatus() {
-        self.locationManager.delegate = self
-        self.locationManager.startUpdatingLocation()
-        
-        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
-            
-            self.myMapView.showsUserLocation = true
-        }else{
-            locationManager.requestWhenInUseAuthorization()
-        }
 
-        
-    }
+
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.checkLocationAuthorizationStatus()
+
     }
 
 
-}
-
-extension LocaisVC: CLLocationManagerDelegate {
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        
-        self.checkLocationAuthorizationStatus()
-    }
 }
